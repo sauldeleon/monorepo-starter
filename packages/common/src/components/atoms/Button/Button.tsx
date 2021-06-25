d88113ed5c6e528dce7b58a623d1bd6be7713664
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2021-06-26 00:40:13 +0200

    chore: update dependencies and storybook

--- packages/common/src/components/atoms/Button/Button.tsx
+++ packages/common/src/components/atoms/Button/Button.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { StyledButton } from './Button.styles'
 
-export type CustomButtonProps = {
+export interface CustomButtonProps extends ButtonProps {
+  /**
+   * text rendered in the button. If given, children are not painted
+   */
   text?: string
-} & ButtonProps
+}
 
+/**
+ * Main Buttom component for the application
+ */
 export const Button: React.FC<CustomButtonProps> = ({ text, children, ...props }) => (
   <StyledButton data-testid="testButton" {...props}>
     {text || children}
